f54b719200fae5a12f6df2b3914fd55099bf849b
NarminAlakbarova <725e5lb@code.edu.az>
2023-05-03 01:31:23 +0400

    added sign-up and sing in page

---
+++ signUp.js
@@ -0,0 +1,25 @@
+let form = document.querySelector("#signform");
+let allInputs = document.querySelectorAll(".form-control");
+let check = document.querySelector("#checkbox");
+let submit = document.querySelector("#submit");
+
+// console.log(allInputs[0]);
+let userData = JSON.parse(localStorage.getItem("userData")) ?? [];
+// let userData=[]
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (check.checked) {
+    if (userData.some((user) => user.email === allInputs[1].value)) {
+      alert("This email already in use.Please use a different email");
+    } else {
+      let userObj = {
+        name: allInputs[0].value,
+        email: allInputs[1].value,
+        password: allInputs[2].value,
+      };
+      userData.push(userObj);
+      localStorage.setItem("userData", JSON.stringify(userData));
+      window.location.href = "http://127.0.0.1:5501/login.html";
+    }
+  }
+});
