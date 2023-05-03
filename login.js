fecc0945ab332955baf8a550f42e603d1c3d4d89
NarminAlakbarova <735e5lb@code.edu.az>
2023-05-03 12:20:53 +0400

    update

---
+++ login.js
@@ -0,0 +1,14 @@
+let form = document.querySelector("#signform");
+let allinp = document.querySelectorAll(".form-control");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let allUser = JSON.parse(localStorage.getItem("userData"));
+  let userObj = allUser.find(
+    (item) => item.name === allinp[0].value && item.password === allinp[1].value
+  );
+  if (userObj) {
+    window.location.href = "http://127.0.0.1:5501/index.html";
+  } else {
+    alert("wrong user");
+  }
+});
