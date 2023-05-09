c2ccdf013a3154d1ce8e3527a92c483884b8c1f0
NarminAlakbarova <735e5lb@code.edu.az>
2023-05-09 12:46:04 +0400

    update

--- New folder/books.js
+++ New folder/books.js
@@ -103,7 +103,7 @@ books.forEach((item) => {
          Publisher: ${item.publisher}
         </p>
          <i><p>Year: ${item.year}</p></i>
-        <button class="btn btn-primary" id=${item.id}>Go somewhere</button>
+        <button class="btn btn-primary" id=${item.id}>Add to Favorites</button>
       </div>
     </div>
   </div>
--- New folder/favorites.js
+++ New folder/favorites.js
@@ -1,6 +1,7 @@
 let favorits = JSON.parse(localStorage.getItem("favoritedBooks")) || [];
 let row = document.querySelector(".row");
 function listFavBooks() {
+  row.innerHTML=""
   favorits.forEach((item) => {
     row.innerHTML += `
         <span class="col-3 my-4">
@@ -11,7 +12,7 @@ function listFavBooks() {
              Publisher: ${item.publisher}
             </p>
              <i><p>Year: ${item.year}</p></i>
-            <button class="btn btn-danger" id=${item.id} onclick=remove(${item.id},this)>Remove from Favorites</button>
+            <button class="btn btn-danger" id=${item.id} onclick=remove("${item.id}",this)>Remove from Favorites</button>
           </div>
         </div>
       </span>
